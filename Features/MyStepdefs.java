bba77a7566601fc0b21bbcad828251738e56adf0
Maxsof Yolo <ks_chabi@esi.dz>
2024-12-30 19:16:14 +0100

    A

---
+++ Features/MyStepdefs.java
@@ -0,0 +1,22 @@
+public class MyStepdefs {
+    public MyStepdefs() {
+        Given("^I have A Matrix$", () -> {
+        });
+        When("^I compute determinant of$", () -> {
+        });
+        Then("^The result of determinant is -(\\d+)$", (Integer arg0) -> {
+        });
+        When("^I compute transpose of$", () -> {
+        });
+        Then("^The result of transpose is$", () -> {
+        });
+        When("^I compute inverse of$", () -> {
+        });
+        Then("^The result of inverse is$", () -> {
+        });
+        When("^I compute cofactor of$", () -> {
+        });
+        Then("^The result of cofactor is$", () -> {
+        });
+    }
+}
